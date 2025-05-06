e84bf193c49e8b76186dd5acd03a75f0887b53b9
roswell <roswell_pando@outlook.es>
2025-05-06 13:02:53 -0500

    starter vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>Calculator App</main>
+  <footer>
+    <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+        >Frontend Mentor</a
+      >. Coded by <a href="#">Your Name Here</a>.
+    </div>
+  </footer>
+</template>
